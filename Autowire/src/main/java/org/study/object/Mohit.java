b41cefbd73106f3a07eebed61da5697745935e6e
brajesh <brajesh.kumar@squareyards.co.in>
2019-09-12 11:23:49 +0530

    autowired super constructor

--- Autowire/src/main/java/org/study/object/Mohit.java
+++ Autowire/src/main/java/org/study/object/Mohit.java
@@ -5,11 +5,23 @@ import org.springframework.stereotype.Component;
 import org.study.interfaces.Man;
 @Component()
 public class Mohit implements Man {
-    @Autowired //this makes to create object  of that class avoid null pointer Exception
+//    @Autowired //this makes to create object  of that class avoid null pointer Exception
+//    @Autowired(required = false) //this makes to create object  of that class avoid null pointer Exception
 	Fruit fruit;
 	
 	
 	
+	
+	@Autowired   
+	public Mohit(Fruit fruit) {
+		super();
+		fruit.setFruitname("Mango");
+		this.fruit=fruit;
+		// TODO Auto-generated constructor stub
+	}
+
+
+
 	@Override
 	public String speak() {
 		// TODO Auto-generated method stub
